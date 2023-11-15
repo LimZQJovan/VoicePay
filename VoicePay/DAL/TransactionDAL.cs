e7550777ecf1d9fdff288c9c56dd02baf5f99629
Royston Lee <s10247568@connect.np.edu.sg>
2023-11-15 19:15:53 +0800

    huat

---
+++ VoicePay/DAL/TransactionDAL.cs
@@ -0,0 +1,26 @@
+﻿using System.Data.SqlClient;
+using System.Xml.Linq;
+using VoicePay.Models;
+
+namespace VoicePay.DAL
+{
+    public class TransactionDAL
+    {
+        private IConfiguration Configuration { get; }
+        private SqlConnection conn;
+        //Constructor
+        public TransactionDAL()
+        {
+            //Read ConnectionString from appsettings.json file
+            var builder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json");
+            Configuration = builder.Build();
+            string strConn = Configuration.GetConnectionString(
+            "NPCSConnectionString");
+            //Instantiate a SqlConnection object with the
+            //Connection String read.
+            conn = new SqlConnection(strConn);
+        }
+    }
+}
