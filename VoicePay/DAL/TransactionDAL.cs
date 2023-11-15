6178ee5f3f5485d780da4b45f6f95d41738b7ad0
Lee Royston <S10247568@connect.np.edu.sg>
2023-11-16 00:26:38 +0800

    -

--- VoicePay/DAL/TransactionDAL.cs
+++ VoicePay/DAL/TransactionDAL.cs
@@ -1,4 +1,5 @@
 ﻿using System.Data.SqlClient;
+using VoicePay.Models;
 
 namespace VoicePay.DAL
 {
@@ -19,6 +20,50 @@ namespace VoicePay.DAL
             //Instantiate a SqlConnection object with the
             //Connection String read.
             conn = new SqlConnection(strConn);
+        }
+
+        public Transaction GetTransactionsByDay(string UEN, int dtYear, int dtMonth, int dtDay)
+        {
+            Transaction transaction = new Transaction();
+            //Create a SqlCommand object from connection object
+            SqlCommand cmd = conn.CreateCommand();
+            //Specify the SELECT SQL statement
+            cmd.CommandText = @"SELECT b.PayeeName,b.[Mobile No], t.SenderAccountNumber, t.Amount, TransactionDateTime
+                                FROM TransactionDetails t
+                                INNER JOIN Stall s
+                                ON s.UEN = t.ReceiverUEN
+                                INNER JOIN BankDetails b
+                                ON b.AccountNumber = t.SenderAccountNumber
+                                WHERE t.ReceiverUEN = @UEN
+                                AND YEAR(TransactionDateTime) = @dtYear
+                                AND MONTH(TransactionDateTime) = @dtMonth
+                                AND DAY(TransactionDateTime) = @dtDay";
+
+            cmd.Parameters.AddWithValue("@UEN", UEN);
+            cmd.Parameters.AddWithValue("@dtYear", dtYear);
+            cmd.Parameters.AddWithValue("@dtMonth", dtMonth);
+            cmd.Parameters.AddWithValue("@dtDay", dtDay);
+            //Open a database connection
+            conn.Open();
+            //Execute the SELECT SQL through a DataReader
+            SqlDataReader reader = cmd.ExecuteReader();
+            if (reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                    transaction.Payee = reader.GetString(0); //0: 1st column
+                    transaction.SenderAccountNumber = reader.GetString(1);  //1: 2nd column
+                    transaction.Amount = reader.GetDecimal(2); //1: 2nd column
+                    transaction.PayeeTelNo = reader.GetString(3);
+                    transaction.TransactionDateTime = reader.GetDateTime(4);            
+                };
+            }
+            //Close DataReader
+            reader.Close();
+            //Close the database connection
+            conn.Close();
+
+            return transaction;
         }
     }
 }
