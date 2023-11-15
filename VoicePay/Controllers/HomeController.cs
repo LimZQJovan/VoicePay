1ec6072ca959bb816dd1097d1df51aebac53904f
LimZQJovan <S10247638@connect.np.edu.sg>
2023-11-15 14:08:38 +0800

    Updated Login

--- VoicePay/Controllers/HomeController.cs
+++ VoicePay/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using VoicePay.DAL;
 using VoicePay.Models;
 
 namespace VoicePay.Controllers
@@ -17,7 +18,44 @@ namespace VoicePay.Controllers
 		{
 			return View();
 		}
+		        [HttpPost]
+        public ActionResult StallLogin(IFormCollection formData)
+        {
+            // Read inputs from textboxes
+            // Email address converted to lowercase
+            string loginID = formData["txtLoginID"].ToString().ToLower();
+            string password = formData["txtPassword"].ToString();
+            StaffDAL staffContext = new StaffDAL();
 
+            if (staffContext.Login(loginID, password))
+            {
+
+                // Store Login ID in session with the key "LoginID"
+                // Store user role "Staff" as a string in session with the key "Role"
+                // Redirect user to the "StaffMain" view through an action
+                return RedirectToAction("StallMain");
+            }
+            else
+            {
+                // Store an error message in TempData for display at the index view
+                TempData["Message"] = "Invalid Login Credentials!";
+                // Redirect user back to the index view through an action
+                return RedirectToAction("Index");
+            }
+        }
+		public ActionResult StallMain()
+		{
+			// Stop accessing the action if not logged in
+			// or account not in the "Staff" role
+
+			return View();
+		}
+		public ActionResult LogOut()
+		{
+			// Clear all key-values pairs stored in session state
+			// Call the Index action of Home controller
+			return RedirectToAction("Index");
+		}
 		public IActionResult Privacy()
 		{
 			return View();
