8f9b8407e15b17c0f3008380318093a7ba17ae21
Royston Lee <s10247568@connect.np.edu.sg>
2023-11-15 17:23:48 +0800

    Please pull - Royston

---
+++ VoicePay/Models/Transaction.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace VoicePay.Models
+{
+	public class Transaction
+	{
+		public string TransactionID { get; set; }
+		public string ReceiverUEN { get; set; }
+		public string SenderAccountNumber { get; set; }
+		public string Amount { get; set; }
+
+
+	}
+}
