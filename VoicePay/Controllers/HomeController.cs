f043915f4329747117f4792c2daf24360cc4ee4a
Royston Lee <s10247568@connect.np.edu.sg>
2023-11-16 14:20:08 +0800

    Transaction

--- VoicePay/Controllers/HomeController.cs
+++ VoicePay/Controllers/HomeController.cs
@@ -25,22 +25,22 @@ namespace VoicePay.Controllers
             // Email address converted to lowercase
             string loginID = formData["txtLoginID"].ToString().ToLower();
             string password = formData["txtPassword"].ToString();
-            string AccUEN = "";
+            string UEN = "";
             string stallName = "";
             string location = "";
 
             StaffDAL staffContext = new StaffDAL();
 
-            if (staffContext.Login(loginID, password, out AccUEN, out stallName, out location))
+            if (staffContext.Login(loginID, password, out UEN, out stallName, out location))
             {
-                HttpContext.Session.SetString("UEN", AccUEN);
+                HttpContext.Session.SetString("UEN", UEN);
                 HttpContext.Session.SetString("Name", stallName);
                 HttpContext.Session.SetString("Location", location);
 
                 // Store Login ID in session with the key "LoginID"
                 // Store user role "Staff" as a string in session with the key "Role"
                 // Redirect user to the "StaffMain" view through an action
-                return RedirectToAction("StallMain");
+                return RedirectToAction("Report");
             }
             else
             {
--- VoicePay/Models/Transaction.cs
+++ VoicePay/Models/Transaction.cs
@@ -2,15 +2,18 @@
 {
     public class Transaction
     {
-        public string TransactionID { get; set; }
-        public string ReceiverUEN { get; set; }
+        public string Payee { get; set; }
+        public string PayeeTelNo { get; set; }
         public string SenderAccountNumber { get; set; }
+        public string TransactionID { get; set; }
         public decimal Amount { get; set; }
 
         public DateTime TransactionDateTime { get; set; }
+        public string ReceiverUEN { get; set; }
+       
+      
         public string ReferenceNo { get; set; }
 
-        public string Payee { get; set;}
-        public string PayeeTelNo { get; set;}
+      
     }
 }
